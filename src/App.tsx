59ae8187a70ccb902ef1b6e5c561e6c98a67cda5
Chachanidze29 <chachanidze29m@gmail.com>
2024-05-31 19:26:28 +0400

    fix issues. add option to select deck size

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import cardsData from "./data/cards.json";
+import cardsData52 from "./data/cards52.json";
+import cardsData36 from "./data/cards36.json";
 import { shuffleCards } from "./utils/shuffle";
 import Multiselect from "multiselect-react-dropdown";
 
@@ -12,7 +13,8 @@ export type Card = {
 type MessageType = "success" | "error";
 
 function App() {
-  const [cardCount, setCardCount] = useState(52);
+  const [deckSize, setDeckSize] = useState(36);
+  const [cardCount, setCardCount] = useState(36);
   const [timer, setTimer] = useState(60);
   const [cards, setCards] = useState<Card[]>([]);
   const [showCards, setShowCards] = useState(false);
@@ -25,10 +27,17 @@ function App() {
 
   let timeoutId: number;
 
+  const handleDeckSizeChange = (size: number) => {
+    setDeckSize(size);
+    setCards;
+  };
+
   const startGame = () => {
     resetGame();
     setShowCards(true);
-    setCards(shuffleCards(cardsData, cardCount));
+    setCards(
+      shuffleCards(deckSize === 36 ? cardsData36 : cardsData52, cardCount)
+    );
     timeoutId = window.setTimeout(() => {
       setShowCards(false);
       setShowSelect(true);
@@ -59,10 +68,32 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="header">CardRecall</h1>
+      <div className="deck-size">
+        <h3>Select Deck Size: {deckSize}</h3>
+        <div className="deck-size-buttons">
+          <button
+            disabled={showCards || showSelect}
+            type="button"
+            className={`button ${deckSize === 36 ? "active" : ""}`}
+            onClick={() => handleDeckSizeChange(36)}
+          >
+            36
+          </button>
+          <button
+            disabled={showCards || showSelect}
+            type="button"
+            className={`button ${deckSize === 52 ? "active" : ""}`}
+            onClick={() => handleDeckSizeChange(52)}
+          >
+            52
+          </button>
+        </div>
+      </div>
       <div className="inputs">
         <div className="input-container">
-          <label htmlFor="count">Card Count {cardCount} (1-52)</label>
+          <label htmlFor="count">
+            Card Count {cardCount} (5-{deckSize})
+          </label>
           <input
             type="number"
             id="count"
@@ -70,7 +101,7 @@ function App() {
             value={cardCount}
             onChange={(e) => {
               const value = Number(e.target.value);
-              if (value <= 52 && value > 0) {
+              if (value <= deckSize && value > 0) {
                 setCardCount(value);
               }
             }}
@@ -125,7 +156,7 @@ function App() {
       {showSelect && (
         <div className="select-container">
           <Multiselect
-            options={cardsData}
+            options={deckSize === 36 ? cardsData36 : cardsData52}
             selectedValues={selectedCards}
             onSelect={(value) => {
               setSelectedCards(value);
